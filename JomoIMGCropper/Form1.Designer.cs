7027d4a781dd2c2859efbd8e1159caf162941518
Brzjomo <zhiqiang_xue@outlook.com>
2024-06-24 23:18:02 +0800

    完善界面和功能

--- JomoIMGCropper/Form1.Designer.cs
+++ JomoIMGCropper/Form1.Designer.cs
@@ -29,11 +29,22 @@
         private void InitializeComponent()
         {
             BTN_Crop = new Button();
+            LB_Input = new Label();
+            LB_Output = new Label();
+            LLB_Input = new LinkLabel();
+            LLB_Output = new LinkLabel();
+            BTN_SelectInput = new Button();
+            BTN_SelectOutput = new Button();
+            CB_SetOutputDirectory = new CheckBox();
+            groupBox1 = new GroupBox();
+            CB_MaxSizeLimit = new CheckBox();
+            TB_MaxSizeLimit = new TextBox();
+            groupBox1.SuspendLayout();
             SuspendLayout();
             // 
             // BTN_Crop
             // 
-            BTN_Crop.Location = new Point(197, 170);
+            BTN_Crop.Location = new Point(292, 277);
             BTN_Crop.Name = "BTN_Crop";
             BTN_Crop.Size = new Size(118, 53);
             BTN_Crop.TabIndex = 0;
@@ -41,21 +52,147 @@
             BTN_Crop.UseVisualStyleBackColor = true;
             BTN_Crop.Click += BTN_Crop_Click;
             // 
+            // LB_Input
+            // 
+            LB_Input.AutoSize = true;
+            LB_Input.Location = new Point(28, 24);
+            LB_Input.Name = "LB_Input";
+            LB_Input.Size = new Size(94, 21);
+            LB_Input.TabIndex = 1;
+            LB_Input.Text = "图片文件夹:";
+            // 
+            // LB_Output
+            // 
+            LB_Output.AutoSize = true;
+            LB_Output.Location = new Point(28, 67);
+            LB_Output.Name = "LB_Output";
+            LB_Output.Size = new Size(94, 21);
+            LB_Output.TabIndex = 2;
+            LB_Output.Text = "输出文件夹:";
+            // 
+            // LLB_Input
+            // 
+            LLB_Input.LinkColor = Color.FromArgb(255, 128, 0);
+            LLB_Input.Location = new Point(128, 24);
+            LLB_Input.Name = "LLB_Input";
+            LLB_Input.Size = new Size(188, 21);
+            LLB_Input.TabIndex = 3;
+            LLB_Input.TabStop = true;
+            LLB_Input.Text = "未选择";
+            LLB_Input.LinkClicked += LLB_Input_LinkClicked;
+            // 
+            // LLB_Output
+            // 
+            LLB_Output.LinkColor = Color.FromArgb(255, 128, 0);
+            LLB_Output.Location = new Point(128, 67);
+            LLB_Output.Name = "LLB_Output";
+            LLB_Output.Size = new Size(188, 21);
+            LLB_Output.TabIndex = 4;
+            LLB_Output.TabStop = true;
+            LLB_Output.Text = "未选择";
+            LLB_Output.LinkClicked += LLB_Output_LinkClicked;
+            // 
+            // BTN_SelectInput
+            // 
+            BTN_SelectInput.Location = new Point(322, 16);
+            BTN_SelectInput.Name = "BTN_SelectInput";
+            BTN_SelectInput.Size = new Size(88, 37);
+            BTN_SelectInput.TabIndex = 5;
+            BTN_SelectInput.Text = "选择";
+            BTN_SelectInput.UseVisualStyleBackColor = true;
+            BTN_SelectInput.Click += BTN_SelectInput_Click;
+            // 
+            // BTN_SelectOutput
+            // 
+            BTN_SelectOutput.Location = new Point(322, 59);
+            BTN_SelectOutput.Name = "BTN_SelectOutput";
+            BTN_SelectOutput.Size = new Size(88, 37);
+            BTN_SelectOutput.TabIndex = 6;
+            BTN_SelectOutput.Text = "选择";
+            BTN_SelectOutput.UseVisualStyleBackColor = true;
+            BTN_SelectOutput.Click += BTN_SelectOutput_Click;
+            // 
+            // CB_SetOutputDirectory
+            // 
+            CB_SetOutputDirectory.AutoSize = true;
+            CB_SetOutputDirectory.Location = new Point(20, 27);
+            CB_SetOutputDirectory.Name = "CB_SetOutputDirectory";
+            CB_SetOutputDirectory.Size = new Size(173, 25);
+            CB_SetOutputDirectory.TabIndex = 7;
+            CB_SetOutputDirectory.Text = "单独设置输出文件夹";
+            CB_SetOutputDirectory.UseVisualStyleBackColor = true;
+            CB_SetOutputDirectory.CheckedChanged += CB_SetOutputDirectory_CheckedChanged;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(TB_MaxSizeLimit);
+            groupBox1.Controls.Add(CB_MaxSizeLimit);
+            groupBox1.Controls.Add(CB_SetOutputDirectory);
+            groupBox1.Location = new Point(28, 122);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(382, 141);
+            groupBox1.TabIndex = 8;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "设置";
+            // 
+            // CB_MaxSizeLimit
+            // 
+            CB_MaxSizeLimit.AutoSize = true;
+            CB_MaxSizeLimit.Location = new Point(20, 58);
+            CB_MaxSizeLimit.Name = "CB_MaxSizeLimit";
+            CB_MaxSizeLimit.Size = new Size(129, 25);
+            CB_MaxSizeLimit.TabIndex = 8;
+            CB_MaxSizeLimit.Text = "限制最大尺寸:";
+            CB_MaxSizeLimit.UseVisualStyleBackColor = true;
+            CB_MaxSizeLimit.CheckedChanged += CB_MaxSizeLimit_CheckedChanged;
+            // 
+            // TB_MaxSizeLimit
+            // 
+            TB_MaxSizeLimit.Location = new Point(153, 57);
+            TB_MaxSizeLimit.Name = "TB_MaxSizeLimit";
+            TB_MaxSizeLimit.Size = new Size(64, 28);
+            TB_MaxSizeLimit.TabIndex = 9;
+            TB_MaxSizeLimit.Text = "512";
+            TB_MaxSizeLimit.TextAlign = HorizontalAlignment.Center;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(10F, 21F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(433, 345);
+            Controls.Add(groupBox1);
+            Controls.Add(BTN_SelectOutput);
+            Controls.Add(BTN_SelectInput);
+            Controls.Add(LLB_Output);
+            Controls.Add(LLB_Input);
+            Controls.Add(LB_Output);
+            Controls.Add(LB_Input);
             Controls.Add(BTN_Crop);
             Font = new Font("Microsoft YaHei UI", 12F);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
             Margin = new Padding(4);
+            MaximizeBox = false;
             Name = "Form1";
-            Text = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Jomo图片裁剪器";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Button BTN_Crop;
+        private Label LB_Input;
+        private Label LB_Output;
+        private LinkLabel LLB_Input;
+        private LinkLabel LLB_Output;
+        private Button BTN_SelectInput;
+        private Button BTN_SelectOutput;
+        private CheckBox CB_SetOutputDirectory;
+        private GroupBox groupBox1;
+        private TextBox TB_MaxSizeLimit;
+        private CheckBox CB_MaxSizeLimit;
     }
 }
--- JomoIMGCropper/Form1.cs
+++ JomoIMGCropper/Form1.cs
@@ -8,29 +8,56 @@ namespace JomoIMGCropper
         public Form1()
         {
             InitializeComponent();
+
+            CB_SetOutputDirectory.Checked = false;
+            LLB_Output.Enabled = false;
+            BTN_SelectOutput.Enabled = false;
+            CB_MaxSizeLimit.Checked = true;
+            TB_MaxSizeLimit.Enabled = true;
+            TB_MaxSizeLimit.Text = "512";
         }
+
+        private static string InputFolder = String.Empty;
+        private static string OutputFolder = String.Empty;
 
         private void CropImages()
         {
-            string inputFolder = "TrainSets"; // 输入文件夹路径
-            string outputFolder = "ProcessedTrainSets"; // 输出文件夹路径
-
-            int maxSize = 512; // 最大宽度或高度
-            int resolution = 72; // 分辨率
-
-            if (!Directory.Exists(inputFolder))
+            if (InputFolder == String.Empty || CB_SetOutputDirectory.Checked && OutputFolder == String.Empty)
             {
-                Directory.CreateDirectory(inputFolder);
-                MessageBox.Show("不存在文件夹" + inputFolder + ",\n请先加需要处理的图片放入该文件夹", "提醒", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                MessageBox.Show("尚未设置输入或输出文件夹" + ",\n请先设置后再运行", "提醒", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                 return;
             }
 
-            if (!Directory.Exists(outputFolder))
+            if (!CB_SetOutputDirectory.Checked)
             {
-                Directory.CreateDirectory(outputFolder);
+                OutputFolder = InputFolder + "\\" + "ProcessedTrainSets";
             }
 
-            string[] files = Directory.GetFiles(inputFolder);
+            int maxSize = 512; // 最大宽度或高度
+
+            try {
+                maxSize = Int16.Parse(TB_MaxSizeLimit.Text);
+            } catch
+            {
+                MessageBox.Show("图片最大尺寸设置错误" + ",\n请检查是否为数字", "提醒", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            int resolution = 72; // 分辨率
+
+            if (!Directory.Exists(InputFolder))
+            {
+                Directory.CreateDirectory(InputFolder);
+                MessageBox.Show("不存在文件夹" + InputFolder + ",\n请先加需要处理的图片放入该文件夹", "提醒", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (!Directory.Exists(OutputFolder))
+            {
+                Directory.CreateDirectory(OutputFolder);
+            }
+
+            string[] files = Directory.GetFiles(InputFolder);
 
             foreach (string file in files)
             {
@@ -40,29 +67,42 @@ namespace JomoIMGCropper
                     {
                         using (Image image = Image.FromStream(stream, false, false))
                         {
-                            int newWidth, newHeight;
-
-                            if (image.Width > image.Height)
+                            if (CB_MaxSizeLimit.Checked)
                             {
-                                newWidth = maxSize;
-                                newHeight = (int)(((double)image.Height / image.Width) * maxSize);
-                            }
-                            else
+                                int newWidth, newHeight;
+
+                                if (image.Width > image.Height)
+                                {
+                                    newWidth = maxSize;
+                                    newHeight = (int)(((double)image.Height / image.Width) * maxSize);
+                                }
+                                else
+                                {
+                                    newHeight = maxSize;
+                                    newWidth = (int)(((double)image.Width / image.Height) * maxSize);
+                                }
+
+                                Bitmap newImage = new Bitmap(maxSize, maxSize);
+                                Graphics graphics = Graphics.FromImage(newImage);
+                                graphics.FillRectangle(Brushes.White, 0, 0, maxSize, maxSize);
+                                graphics.DrawImage(image, (maxSize - newWidth) / 2, (maxSize - newHeight) / 2, newWidth, newHeight);
+
+                                string outputFilePath = Path.Combine(OutputFolder, Path.GetFileName(file));
+                                string[] _ = outputFilePath.Split('.');
+                                outputFilePath = _[0] + ".jpeg";
+                                newImage.SetResolution(resolution, resolution);
+                                newImage.Save(outputFilePath, ImageFormat.Jpeg);
+                            } else
                             {
-                                newHeight = maxSize;
-                                newWidth = (int)(((double)image.Width / image.Height) * maxSize);
+                                string outputFilePath = Path.Combine(OutputFolder, Path.GetFileName(file));
+                                string[] _ = outputFilePath.Split('.');
+                                outputFilePath = _[0] + ".jpeg";
+                                Bitmap newImage = new Bitmap(image.Width, image.Height);
+                                Graphics graphics = Graphics.FromImage(newImage);
+                                graphics.DrawImage(image, 0, 0, image.Width, image.Height);
+                                newImage.SetResolution(resolution, resolution);
+                                newImage.Save(outputFilePath, ImageFormat.Jpeg);
                             }
-
-                            Bitmap newImage = new Bitmap(maxSize, maxSize);
-                            Graphics graphics = Graphics.FromImage(newImage);
-                            graphics.FillRectangle(Brushes.White, 0, 0, maxSize, maxSize);
-                            graphics.DrawImage(image, (maxSize - newWidth) / 2, (maxSize - newHeight) / 2, newWidth, newHeight);
-
-                            string outputFilePath = Path.Combine(outputFolder, Path.GetFileName(file));
-                            string[] _ = outputFilePath.Split('.');
-                            outputFilePath = _[0] + ".jpeg";
-                            newImage.SetResolution(resolution, resolution);
-                            newImage.Save(outputFilePath, ImageFormat.Jpeg);
                         }
                     }
                 }
@@ -71,38 +111,143 @@ namespace JomoIMGCropper
                     // 对于不支持的图片，使用FreeImage库处理
                     FIBITMAP bitmap = FreeImage.LoadEx(file);
 
-                    if (!bitmap.IsNull)
+                    if (CB_MaxSizeLimit.Checked)
                     {
-                        int width = (int)FreeImage.GetWidth(bitmap);
-                        int height = (int)FreeImage.GetHeight(bitmap);
+                        if (!bitmap.IsNull)
+                        {
+                            int width = (int)FreeImage.GetWidth(bitmap);
+                            int height = (int)FreeImage.GetHeight(bitmap);
 
-                        double aspectRatio = (double)width / height;
-                        int newWidth = width > height ? maxSize : (int)(maxSize * aspectRatio);
-                        int newHeight = width > height ? (int)(maxSize / aspectRatio) : maxSize;
+                            double aspectRatio = (double)width / height;
+                            int newWidth = width > height ? maxSize : (int)(maxSize * aspectRatio);
+                            int newHeight = width > height ? (int)(maxSize / aspectRatio) : maxSize;
 
-                        Bitmap newImage = new Bitmap(maxSize, maxSize);
-                        Graphics graphics = Graphics.FromImage(newImage);
-                        graphics.FillRectangle(Brushes.White, 0, 0, maxSize, maxSize);
-                        Image image = FreeImage.GetBitmap(bitmap);
-                        graphics.DrawImage(image, (maxSize - newWidth) / 2, (maxSize - newHeight) / 2, newWidth, newHeight);
+                            Bitmap newImage = new Bitmap(maxSize, maxSize);
+                            Graphics graphics = Graphics.FromImage(newImage);
+                            graphics.FillRectangle(Brushes.White, 0, 0, maxSize, maxSize);
+                            Image image = FreeImage.GetBitmap(bitmap);
+                            graphics.DrawImage(image, (maxSize - newWidth) / 2, (maxSize - newHeight) / 2, newWidth, newHeight);
 
-                        string outputFilePath = Path.Combine(outputFolder, Path.GetFileName(file));
-                        string[] _ = outputFilePath.Split('.');
-                        outputFilePath = _[0] + ".jpeg";
-                        newImage.SetResolution(resolution, resolution);
-                        newImage.Save(outputFilePath, ImageFormat.Jpeg);
+                            string outputFilePath = Path.Combine(OutputFolder, Path.GetFileName(file));
+                            string[] _ = outputFilePath.Split('.');
+                            outputFilePath = _[0] + ".jpeg";
+                            newImage.SetResolution(resolution, resolution);
+                            newImage.Save(outputFilePath, ImageFormat.Jpeg);
 
-                        FreeImage.Unload(bitmap);
+                            FreeImage.Unload(bitmap);
+                        }
+                    } else
+                    {
+                        if (!bitmap.IsNull)
+                        {
+                            int width = (int)FreeImage.GetWidth(bitmap);
+                            int height = (int)FreeImage.GetHeight(bitmap);
+
+                            Bitmap newImage = new Bitmap(width, height);
+                            Graphics graphics = Graphics.FromImage(newImage);
+                            Image image = FreeImage.GetBitmap(bitmap);
+                            graphics.DrawImage(image, 0, 0, width, height);
+
+                            string outputFilePath = Path.Combine(OutputFolder, Path.GetFileName(file));
+                            string[] _ = outputFilePath.Split('.');
+                            outputFilePath = _[0] + ".jpeg";
+                            newImage.SetResolution(resolution, resolution);
+                            newImage.Save(outputFilePath, ImageFormat.Jpeg);
+
+                            FreeImage.Unload(bitmap);
+                        }
                     }
                 }
             }
+
+            MessageBox.Show("处理完成!", "提醒", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
 
         private void BTN_Crop_Click(object sender, EventArgs e)
         {
             CropImages();
+        }
 
-            MessageBox.Show("处理完成!", "提醒", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        private void LLB_Input_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            if (InputFolder != "")
+            {
+                try
+                {
+                    System.Diagnostics.Process.Start("explorer", InputFolder);
+                }
+                catch
+                {
+                    MessageBox.Show("路径错误！请确认路径是否存在！", "路径错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void LLB_Output_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            if (OutputFolder != "")
+            {
+                try
+                {
+                    System.Diagnostics.Process.Start("explorer", OutputFolder);
+                }
+                catch
+                {
+                    MessageBox.Show("路径错误！请确认路径是否存在！", "路径错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void BTN_SelectInput_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new();
+
+            DialogResult result = folderBrowserDialog.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                InputFolder = folderBrowserDialog.SelectedPath;
+                LLB_Input.Text = InputFolder.Split('\\')[^1];
+            }
+        }
+
+        private void BTN_SelectOutput_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new();
+
+            DialogResult result = folderBrowserDialog.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                OutputFolder = folderBrowserDialog.SelectedPath;
+                LLB_Output.Text = OutputFolder.Split('\\')[^1];
+            }
+        }
+
+        private void CB_SetOutputDirectory_CheckedChanged(object sender, EventArgs e)
+        {
+            if (CB_SetOutputDirectory.Checked)
+            {
+                LLB_Output.Enabled = true;
+                BTN_SelectOutput.Enabled = true;
+            }
+            else
+            {
+                LLB_Output.Enabled = false;
+                BTN_SelectOutput.Enabled = false;
+            }
+        }
+
+        private void CB_MaxSizeLimit_CheckedChanged(object sender, EventArgs e)
+        {
+            if (CB_MaxSizeLimit.Checked)
+            {
+                TB_MaxSizeLimit.Enabled = true;
+            }
+            else
+            {
+                TB_MaxSizeLimit.Enabled = false;
+            }
         }
     }
 }
